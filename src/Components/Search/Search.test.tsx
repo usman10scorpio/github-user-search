a028b9a170d60727c0b0fadd13cf6d19a4a0454a
usman10scorpio <usman10scorpio@gmail.com>
2022-11-23 16:18:50 +0500

    main :: write test cases for search component

---
+++ src/Components/Search/Search.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import Search from './Search';
+
+
+test('React search component find heading text', () => {
+  render(<Search  />);
+  const linkElement = screen.getByText(/Github Search/i) || screen.getByText(/GS/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('React search component find placeholder text in input tag', () => {
+    render(<Search  />);
+    const linkElement = screen.getByPlaceholderText('Enter the user you want to search ...')
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('React search component find search text in button tag', () => {
+    render(<Search  />);
+    const linkElement = screen.getByRole('button', { name: /Search/i})
+    expect(linkElement).toBeInTheDocument();
+});

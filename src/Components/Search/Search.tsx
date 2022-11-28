916ff1aeb075870539967e65dc884e0ee9132764
usman10scorpio <usman10scorpio@gmail.com>
2022-11-28 17:05:05 +0500

    main :: regex text added in main screen

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -27,6 +27,14 @@ const Search = ({
               type="submit">
               Search
          </button>
+        <div  className={(success ?  "search__validation--hide" : "search__validation--show")}>
+          <ul>
+            <li>Github username may only contain alphanumeric characters or hyphens.</li>
+            <li>Github username cannot have multiple consecutive hyphens.</li>
+            <li>Github username cannot begin or end with a hyphen.</li>
+            <li>Maximum is 39 characters.</li>
+          </ul>
+        </div>
       </form>
     </div>
   );
